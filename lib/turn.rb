9e69d2afed8b807c0d80182627185d0fc2d6889f
Larissa S <larissafschiavo@gmail.com>
2017-10-17 04:52:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,10 +11,9 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board, index)
-  if index < 9 && position_taken?(board, index) == false 
-    return true
-  else
-    return false
+  if position_taken?(board, index)
+    return false 
+  
   end
 end
 
